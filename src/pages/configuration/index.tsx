dd8224ce6b96ea76e41cc0804a4042a51ae2e150
LuanEnrico30 <luanzinhoenrico@gmail.com>
2025-05-06 17:02:35 -0400

    fix: tela de configuração

--- src/pages/configuration/index.tsx
+++ src/pages/configuration/index.tsx
@@ -8,11 +8,34 @@ import { Slider } from "@/components/ui/slider"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Heading } from "@/components/ui/heading"
 import { LayoutHome } from "@/layout/LayoutHome"
-import { Switch } from "@/components/ui/switch"
 
 export function Configuration() {
   const [soundVolume, setSoundVolume] = useState(75)
   const [musicVolume, setMusicVolume] = useState(50)
+  const [notifications, setNotifications] = useState({
+    promocoes: true,
+    atualizacoes: false,
+    jogos: false,
+    torneios: false,
+  })
+  const [security, setSecurity] = useState({
+    doisFatores: false,
+    verificacaoLogin: true,
+  })
+
+  const CustomSwitch = ({ checked, onChange }) => (
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+        className="sr-only"
+      />
+      <div className={`w-11 h-6 rounded-full transition-colors duration-200 ${checked ? 'bg-yellow-500' : 'bg-black'}`}>
+        <span className={`absolute left-1 top-1 h-4 w-4 bg-white rounded-full transition-transform duration-200 ${checked ? 'translate-x-5' : ''}`} />
+      </div>
+    </label>
+  )
 
   return (
     <LayoutHome>
@@ -20,14 +43,14 @@ export function Configuration() {
         <Heading className="flex w-full font-bold text-white">
           Configurações
         </Heading>
-        <Button className="w-80 mr-[610px] rounded">
+        <Button className="w-80 mr-[610px] bg-yellow-500 hover:bg-yellow-500 text-black">
           Salvar Alterações
         </Button>
       </div>
 
-      <div className="flex flex-col gap-6 w-full">
+      <div className="flex flex-col mt-7 gap-6 w-full">
         {/* Notificações */}
-        <Card className="border border-solid w-full border-border border-r-[#07080D] border-t-[#07080D] border-b-[#07080D] border-l-[#07080D] bg-gradient-to-b from-[#07080D] to-[#1D1F2C] max-w-[55rem] shadow-xl shadow-blue-gray-900/5">
+        <Card className="bg-[#282B3B] border border-[#45474F] w-full  max-w-[55rem] shadow-xl shadow-blue-gray-900/5">
           <CardContent className="p-4">
             <div className="flex items-center justify-between mb-4">
               <div className="flex items-center gap-2">
@@ -40,26 +63,38 @@ export function Configuration() {
             <div className="grid gap-3">
               <div className="flex items-center justify-between">
                 <span className="text-gray-300 text-sm">Promoções e bônus</span>
-                <Switch />
+                <CustomSwitch
+                  checked={notifications.promocoes}
+                  onChange={() => setNotifications({ ...notifications, promocoes: !notifications.promocoes })}
+                />
               </div>
               <div className="flex items-center justify-between">
                 <span className="text-gray-300 text-sm">Atualizações do sistema</span>
-                <Switch />
+                <CustomSwitch
+                  checked={notifications.atualizacoes}
+                  onChange={() => setNotifications({ ...notifications, atualizacoes: !notifications.atualizacoes })}
+                />
               </div>
               <div className="flex items-center justify-between">
                 <span className="text-gray-300 text-sm">Novos jogos</span>
-                <Switch />
+                <CustomSwitch
+                  checked={notifications.jogos}
+                  onChange={() => setNotifications({ ...notifications, jogos: !notifications.jogos })}
+                />
               </div>
               <div className="flex items-center justify-between">
                 <span className="text-gray-300 text-sm">Torneios</span>
-                <Switch />
+                <CustomSwitch
+                  checked={notifications.torneios}
+                  onChange={() => setNotifications({ ...notifications, torneios: !notifications.torneios })}
+                />
               </div>
             </div>
           </CardContent>
         </Card>
 
         {/* Som e Áudio */}
-        <Card className="border border-solid border-border border-r-[#07080D] border-t-[#07080D] border-b-[#07080D] border-l-[#07080D] bg-gradient-to-b from-[#07080D] to-[#1D1F2C] max-w-[55rem] shadow-xl shadow-blue-gray-900/5">
+        <Card className="bg-[#282B3B] border border-[#45474F] max-w-[55rem] shadow-xl shadow-blue-gray-900/5">
           <CardContent className="p-4">
             <div className="flex items-center justify-between mb-4">
               <div className="flex items-center gap-2">
@@ -73,7 +108,6 @@ export function Configuration() {
               <div className="grid gap-1">
                 <div className="flex items-center justify-between">
                   <span className="text-gray-300 text-sm">Efeitos sonoros</span>
-                  <Switch defaultChecked className="data-[state=checked]:bg-yellow-500" />
                 </div>
                 <div className="pt-2">
                   <Slider
@@ -93,7 +127,6 @@ export function Configuration() {
               <div className="grid gap-1">
                 <div className="flex items-center justify-between">
                   <span className="text-gray-300 text-sm">Música de fundo</span>
-                  <Switch defaultChecked className="data-[state=checked]:bg-yellow-500" />
                 </div>
                 <div className="pt-2">
                   <Slider
@@ -115,7 +148,7 @@ export function Configuration() {
         </Card>
 
         {/* Segurança */}
-        <Card className="border border-solid border-border border-r-[#07080D] border-t-[#07080D] border-b-[#07080D] border-l-[#07080D] bg-gradient-to-b from-[#07080D] to-[#1D1F2C] max-w-[55rem] shadow-xl shadow-blue-gray-900/5">
+        <Card className="bg-[#282B3B] border border-[#45474F] max-w-[55rem] shadow-xl shadow-blue-gray-900/5">
           <CardContent className="p-4">
             <div className="flex items-center justify-between mb-4">
               <div className="flex items-center gap-2">
@@ -123,21 +156,26 @@ export function Configuration() {
                   <Lock className="h-4 w-4 text-black" />
                 </div>
                 <h2 className="text-lg font-semibold text-yellow-500">Segurança</h2>
-              </div>
-              <ChevronRight className="h-4 w-4 text-yellow-500" />
+              </div>  
             </div>
             <div className="grid gap-3">
               <div className="flex items-center justify-between">
                 <span className="text-gray-300 text-sm">Autenticação de dois fatores</span>
-                <Switch className="data-[state=checked]:bg-yellow-500" />
+                <CustomSwitch
+                  checked={security.doisFatores}
+                  onChange={() => setSecurity({ ...security, doisFatores: !security.doisFatores })}
+                />
               </div>
               <div className="flex items-center justify-between">
                 <span className="text-gray-300 text-sm">Verificação de login</span>
-                <Switch defaultChecked className="data-[state=checked]:bg-yellow-500" />
+                <CustomSwitch
+                  checked={security.verificacaoLogin}
+                  onChange={() => setSecurity({ ...security, verificacaoLogin: !security.verificacaoLogin })}
+                />
               </div>
               <div className="flex items-center justify-between">
                 <span className="text-gray-300 text-sm">Alterar senha</span>
-                <Button className=" text-sm py-1 px-3">
+                <Button className="border-yellow-500 text-black hover:bg-white text-sm py-1 px-3">
                   Alterar
                 </Button>
               </div>
@@ -146,7 +184,7 @@ export function Configuration() {
         </Card>
 
         {/* Idioma */}
-        <Card className="border border-solid border-border border-r-[#07080D] border-t-[#07080D] border-b-[#07080D] border-l-[#07080D] bg-gradient-to-b from-[#07080D] to-[#1D1F2C] max-w-[55rem] shadow-xl shadow-blue-gray-900/5">
+        <Card className="bg-[#282B3B] border border-[#45474F] max-w-[55rem] shadow-xl shadow-blue-gray-900/5">
           <CardContent className="p-4">
             <div className="flex items-center justify-between mb-4">
               <div className="flex items-center gap-2">
@@ -160,10 +198,10 @@ export function Configuration() {
               <div className="flex items-center justify-between">
                 <span className="text-gray-300 text-sm">Idioma do sistema</span>
                 <Select defaultValue="pt-BR">
-                  <SelectTrigger className="w-28 bg-zinc-800 border-yellow-400/30 text-yellow-500 text-sm">
+                  <SelectTrigger className="w-28 bg-yellow-500 border-yellow-400/30 text-black text-sm">
                     <SelectValue />
                   </SelectTrigger>
-                  <SelectContent className="bg-zinc-800 border-yellow-400/30">
+                  <SelectContent className="bg-black text-yellow-500 border-yellow-400/30">
                     <SelectItem value="pt-BR">Português</SelectItem>
                     <SelectItem value="en-US">English</SelectItem>
                     <SelectItem value="es-ES">Español</SelectItem>
